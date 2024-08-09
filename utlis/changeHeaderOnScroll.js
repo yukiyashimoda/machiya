031374dc2bf0040274c502e8326b89609198ef4a
apple <apple@appurunoMacBook-Pro.local>
2024-08-09 15:44:22 +0900

    “tex”

--- utlis/changeHeaderOnScroll.js
+++ utlis/changeHeaderOnScroll.js
@@ -1,34 +1,16 @@
-import React, { useEffect } from 'react';
-
-const MyComponent = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      var mainNav = document.querySelector(".main-nav");
-      var navLogoWrapLogo = document.querySelector(".nav-logo-wrap .logo");
-      var lightAfterScroll = document.querySelector(".light-after-scroll");
-
-      if (window.scrollY > 0) {
-        mainNav.classList.remove("transparent");
-        mainNav.classList.add("small-height", "body-scrolled");
-        if (navLogoWrapLogo) navLogoWrapLogo.classList.add("small-height");
-        if (lightAfterScroll) lightAfterScroll.classList.remove("dark");
-      } else if (window.scrollY === 0) {
-        mainNav.classList.add("transparent");
-        mainNav.classList.remove("small-height", "body-scrolled");
-        if (navLogoWrapLogo) navLogoWrapLogo.classList.remove("small-height");
-        if (lightAfterScroll) lightAfterScroll.classList.add("dark");
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Cleanup on unmount
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return <div>My Component</div>;
+export const headerChangeOnScroll = () => {
+  var mainNav = document.querySelector(".main-nav");
+  var navLogoWrapLogo = document.querySelector(".nav-logo-wrap .logo");
+  var lightAfterScroll = document.querySelector(".light-after-scroll");
+  if (window.scrollY > 0) {
+    mainNav.classList.remove("transparent");
+    mainNav.classList.add("small-height", "body-scrolled");
+    if (navLogoWrapLogo) navLogoWrapLogo.classList.add("small-height");
+    if (lightAfterScroll) lightAfterScroll.classList.remove("dark");
+  } else if (window.scrollY === 0) {
+    mainNav.classList.add("transparent");
+    mainNav.classList.remove("small-height", "body-scrolled");
+    if (navLogoWrapLogo) navLogoWrapLogo.classList.remove("small-height");
+    if (lightAfterScroll) lightAfterScroll.classList.add("dark");
+  }
 };
-
-export default MyComponent;
