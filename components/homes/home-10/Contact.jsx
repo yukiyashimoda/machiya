e8818865ae65c62634588b3d73ba4b2ca7ca74a4
apple <apple@appurunoMacBook-Pro.local>
2024-08-12 01:44:06 +0900

    “tex”

--- components/homes/home-10/Contact.jsx
+++ components/homes/home-10/Contact.jsx
@@ -8,9 +8,6 @@ export default function Contact() {
     <div className="container font-alt">
       <div className="row mb-90 mb-sm-50">
         <div className="col-lg-5 col-xl-4 mb-md-90 mb-sm-50 wow fadeInUp">
-          <h2 className="section-title-strong mb-40">
-            <span className="font-alt">求人情報</span>
-          </h2>
           <p className="mb-0 sub4">
             うなぎと酒 まちやでは現在、アルバイトスタッフを募集しています。<br />ご興味のある方は下記の要項をご確認の上、電話もしくはメールにてご連絡ください。
           </p>
