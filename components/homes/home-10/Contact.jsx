56d70722ea3d3fd1dde5ea5a97d514f99a68183c
apple <apple@appurunoMacBook-Pro.local>
2024-08-12 01:43:31 +0900

    “tex”

--- components/homes/home-10/Contact.jsx
+++ components/homes/home-10/Contact.jsx
@@ -5,14 +5,14 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="container">
+    <div className="container font-alt">
       <div className="row mb-90 mb-sm-50">
         <div className="col-lg-5 col-xl-4 mb-md-90 mb-sm-50 wow fadeInUp">
           <h2 className="section-title-strong mb-40">
             <span className="font-alt">求人情報</span>
           </h2>
-          <p className="mb-0">
-            うなぎと酒 まちやでは現在、アルバイトスタッフを募集しています。ご興味のある方は下記の詳細をご確認の上、電話もしくはメールにてご連絡ください。
+          <p className="mb-0 sub4">
+            うなぎと酒 まちやでは現在、アルバイトスタッフを募集しています。<br />ご興味のある方は下記の要項をご確認の上、電話もしくはメールにてご連絡ください。
           </p>
         </div>
         <div className="col-lg-7 offset-xl-1">
@@ -35,27 +35,27 @@ export default function Contact() {
           <hr />
           <li className="item">
             <span className="sub3">業務内容</span>
-            <span className="sub5">ホールスタッフ(簡単な調理補助も)</span>
+            <span className="sub4">ホールスタッフ(簡単な調理補助も)</span>
           </li>
           <hr />
           <li className="item">
             <span className="sub3">勤務時間</span>
-            <span className="sub5">17:00〜22:00</span>
+            <span className="sub4">17:00〜22:00</span>
           </li>
           <hr />
           <li className="item">
             <span className="sub3">時給</span>
-            <span className="sub5">¥1,100(経験により要相談)</span>
+            <span className="sub4">¥1,100(経験により要相談)</span>
           </li>
           <hr />
           <li className="item">
             <span className="sub3">勤務日</span>
-            <span className="sub5">シフト制：平日・土日祝</span>
+            <span className="sub4">シフト制：平日・土日祝</span>
           </li>
           <hr />
           <li className="item">
             <span className="sub3">特記事項</span>
-            <span className="sub5">賄いあり・交通費支給</span>
+            <span className="sub4">賄いあり・交通費支給</span>
           </li>
           <hr />
         </ul>
@@ -63,11 +63,11 @@ export default function Contact() {
         {/* ボタンの追加 */}
         <div className="text-center mt-4">
           {/* 電話をかけるボタン */}
-          <a href="tel:011-600-1747" className="btn btn-primary me-2">
+          <a href="tel:011-600-1747" className="btn btn-primary me-2 sub4">
             電話をかける
           </a>
           {/* メールを送るボタン */}
-          <a href="mailto:info@unagi-machiya.com" className="btn btn-secondary">
+          <a href="mailto:info@unagi-machiya.com" className="btn btn-secondary sub4">
             メールを送る
           </a>
         </div>
