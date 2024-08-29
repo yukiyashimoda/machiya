4f692efad0b9b523fd1eaa861b0f4b7231b3f077
apple <apple@appurunoMacBook-Pro.local>
2024-08-30 04:55:13 +0900

    “reserve”

--- components/homes/home-10/heros/Hero1.jsx
+++ components/homes/home-10/heros/Hero1.jsx
@@ -1,23 +1,88 @@
-import AnimatedText from "@/components/common/AnimatedText";
-import React from "react";
+import React, { useState } from "react";
+
+function ReserveModal({ isOpen, onClose }) {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "15%",
+        bottom: "10px",
+        left: "0",
+        width: "100%",
+        height: "85%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        zIndex: 1000,
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "90%",
+          maxWidth: "800px",
+          height: "100%",
+          backgroundColor: "#fff",
+          padding: "30px",
+          borderRadius: "8px",
+        }}
+      >
+        <button
+          onClick={onClose}
+          style={{
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            background: "rgba(0, 0, 0, 0.5)",
+            border: "none",
+            fontSize: "1.5rem",
+            cursor: "pointer",
+            color: "white",
+          }}
+        >
+          close&times;
+        </button>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://select-type.com/rsv/?id=304fIyuZ3Ko&w_flg=1"
+          frameBorder="0"
+          style={{ borderRadius: "8px" }}
+        ></iframe>
+      </div>
+    </div>
+  );
+}
 
 export default function Hero1() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
     <div className="container min-height-100vh d-flex align-items-center pt-100 pb-100 pt-sm-120 pb-sm-120">
       {/* Home Section Content */}
       <div className="home-content text-center">
-      <span className="charsAnimIn-1" data-splitting="chars">
-        <img src="/assets/images/machiyalogo.png" alt="Test" />
-      </span>
+        <span className="charsAnimIn-1" data-splitting="chars">
+          <img src="/assets/images/machiyalogo.png" alt="Test" />
+        </span>
 
         <div className="local-scroll fadeInUpShort" data-wow-delay="0.57s">
-          <a
-            href="https://maps.app.goo.gl/CWg7GbWtVzE41GoG9"
+          <button
+            onClick={openModal}
             className="link-hover-anim link-circle-1 align-middle"
-            data-link-animate="y"
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+            }}
           >
             <span className="font-alt link-strong link-strong-unhovered">
-              地図を開く{" "}
+              ご予約はこちら{" "}
               <i
                 className="mi-arrow-right size-18 align-middle"
                 aria-hidden="true"
@@ -27,13 +92,13 @@ export default function Hero1() {
               className="font-alt link-strong link-strong-hovered"
               aria-hidden="true"
             >
-              地図を開く{" "}
+              ご予約はこちら{" "}
               <i
                 className="mi-arrow-right size-18 align-middle"
                 aria-hidden="true"
               ></i>
             </span>
-          </a>
+          </button>
         </div>
       </div>
       {/* End Home Section Content */}
@@ -47,6 +112,7 @@ export default function Hero1() {
         </a>
       </div>
       {/* End Scroll Down */}
+      <ReserveModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 }
