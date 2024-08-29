1d824bb1906ca866d986566d23aabc13667f7cab
apple <apple@appurunoMacBook-Pro.local>
2024-08-30 02:41:37 +0900

    “reserve”

--- components/homes/home-10/Reserve.jsx
+++ components/homes/home-10/Reserve.jsx
@@ -1,10 +1,33 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function Reserve() {
+  const iframeRef = useRef(null); // iframeに対する参照を作成
+
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    let lastSrc = iframe.src;
+
+    const intervalId = setInterval(() => {
+      if (iframe.src !== lastSrc) {
+        lastSrc = iframe.src;
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }
+    }, 500);
+
+    // クリーンアップ: コンポーネントがアンマウントされるときにintervalをクリア
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="p-0 m-0">
-    <iframe width="100%" height="1500px" src="https://select-type.com/rsv/?id=304fIyuZ3Ko&w_flg=1" frameborder="0"></iframe>
+      <iframe
+        ref={iframeRef}
+        width="100%"
+        height="1500px"
+        src="https://select-type.com/rsv/?id=304fIyuZ3Ko&w_flg=1"
+        frameBorder="0"
+      ></iframe>
     </div>
   );
 }
